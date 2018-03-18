5bb9059d4168334a0620430b7929e87022fe8ffc
Ahmed <adhoms@adhoms.public.domain.not.really>
2018-03-18 17:12:18 +0200

    General updates

--- car.py
+++ car.py
@@ -7,10 +7,11 @@ import cv2
 bvalues = boxvalues.Values()
 #cvalues = carvalues.Values()
 
+while bvalues.picture is None or bvalues.custom is None:
+	pass
+
 while True:
-	if values.picture is not None and values.custom is not None:
-		while True:
-			#cvalues.feed(bvalues.coordinates.angle)
-			cv2.imshow('picture', bvalues.picture)
-			cv2.imshow('custom', bvalues.custom)
-		        cv2.waitKey(27) & 0xFF == ord('q')
+	#cvalues.feed(bvalues.coordinates.angle)
+	cv2.imshow('picture', bvalues.picture)
+	cv2.imshow('custom', bvalues.custom)
+        cv2.waitKey(27) & 0xFF == ord('q')
